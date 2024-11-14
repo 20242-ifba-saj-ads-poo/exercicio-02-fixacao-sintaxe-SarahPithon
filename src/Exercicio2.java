fdd16500d6a9226a82c73cf212d627e6b829a9c4
SarahPithon <110790276+SarahPithon@users.noreply.github.com>
2024-11-14 23:03:34 +0000

    Segue Exercício2

---
+++ src/Exercicio2.java
@@ -0,0 +1,9 @@
+class Exercicio2 {
+    public static void main(String[] args) {
+        int j=0;
+                for  (int i=1; i <=1000; i++){
+            j+=i;
+            System.out.println(j);
+        }
+    }
+}
---
+++ src/Exercicio3.java
@@ -0,0 +1,9 @@
+class Exercicio3 {
+    public static void main(String[] args) {
+    for (int i=1; 1<=100; i++){
+        if(i/3){
+            System.out.println(i);
+        }
+      }
+    }
+}
